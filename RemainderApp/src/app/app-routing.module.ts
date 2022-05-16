c4d74930823b9a5096ad184434629e300f5c22de
Ajith Suresh <ajithsureshm97@gmail.com>
2022-05-16 21:12:03 +0530

    websiteCompleted

--- RemainderApp/src/app/app-routing.module.ts
+++ RemainderApp/src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
 
 const routes: Routes = [
 
   {
-    path:'',component:LoginComponent
+    path: '', component: LoginComponent
   },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'viewEvent', component: ViewDetailsComponent
+  }
 ];
 
 @NgModule({
--- RemainderApp/src/app/login/login.component.ts
+++ RemainderApp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+
+    userId: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    pwd: ['', [Validators.required, Validators.pattern('[0-9]*')]]
+  })
+
+  constructor(private fb: FormBuilder, private ds: DataService, private router: Router) { }
 
   ngOnInit(): void {
+
+
+  }
+
+  login() {
+
+    var userId = this.loginForm.value.userId
+    var pwd = this.loginForm.value.pwd
+
+    if (this.loginForm.valid) {
+
+      // call dataservice
+
+      this.ds.login(userId, pwd)
+        .subscribe((result: any) => {
+
+          if (result) {
+
+            localStorage.setItem('currentUser', JSON.stringify(result.currentUser))
+            localStorage.setItem('currentUserId', JSON.stringify(result.currentUserId))
+            localStorage.setItem('token', JSON.stringify(result.token))
+
+            alert(result.message)
+
+            this.router.navigateByUrl('dashboard')
+          }
+        },
+          (result: any) => {
+            alert(result.error.message)
+          })
+
+    } else {
+      alert("invalid form")
+    }
   }
 
 }
